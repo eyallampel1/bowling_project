9cc3e621325c831b169111ef6e2b0fea6b12955f
eyallampel1 <eyal.lampel@gmail.com>
2017-04-30 22:51:39 +0300

    this is ver1 of myAngular2QuickStart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MainWindowComponent } from './main-window/main-window.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: MainWindowComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainWindowComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
